41c4d378f3127ce708f1ced2aa913d1a02d5762a
luisgarci4 <luisg@tlapps.mx>
2025-04-14 16:03:50 -0600

    agregar PrincipalConductor

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:raitec/pages/ISConductores.dart';
+import 'package:raitec/pages/PrincipalConductor.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ISConductores(),
+      home: const PrincipalConductor(),
     );
   }
 }
