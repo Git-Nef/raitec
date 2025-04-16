c9598c7d89be93e5bcf138071bec7975b164f33d
luisgarci4 <luisg@tlapps.mx>
2025-04-16 16:44:56 -0600

    merge esteban 2

--- lib/pages/splashscreen.dart
+++ lib/pages/splashscreen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:raitec/pages/home.dart';
 import 'package:raitec/pages/informacionusuarioscreen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -17,7 +16,7 @@ class _MySplashScreen extends State<SplashScreen> {
     super.initState();
     Timer(
       const Duration(seconds: 5), // Se agregó const para optimización
-          () {
+      () {
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(
             builder: (BuildContext context) {
