6c178b8067f7a714b1af064679c65e5dfee63b09
luisgarci4 <luisg@tlapps.mx>
2025-04-23 14:57:48 -0600

    Agregar coleccion para Registro.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:raitec/pages/splashscreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:raitec/pages/Splashscreen.dart';
+import 'firebase_options.dart'; // Este archivo lo genera FlutterFire automáticamente
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'RaiTec',
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: SplashScreen(), // Tu pantalla inicial
     );
   }
 }
